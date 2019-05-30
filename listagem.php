7269e38c9adc9256036b3df0ccecbb05f2f6337e
SamyMartins98 <samymartins98@gmail.com>
2019-05-30 13:48:01 -0300

    Listagem

--- listagem.php
+++ listagem.php
@@ -1,3 +1,9 @@
+<?php
+session_start();
+include("folhaDePagamento-api/Conn/index.php");
+$resultado_folha = $conexao->query("SELECT * FROM folhaDePagamento",PDO::FETCH_ASSOC);
+$resultado_folha->execute();
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -16,20 +22,20 @@
           <a href="#" class="brand-logo logonav"><img src="img/icon.png" style="width: 40px; height: 40px;"></a>
           <ul id="nav-mobile" class="right hide-on-med-and-down">
             <li><a href="index.html">Folha de pagamento</a></li>
-            <li><a href="listagem.html">Listagem</a></li>
+            <li><a href="listagem.php">Listagem</a></li>
           </ul>
         </div>
       </nav>
       <div class="container">
 	 		<div class="row">
-	 			<div class="col s12">
+	 			<div class="col s12 m4">
 					 <table class="striped">
 					 	
 				        <thead>
 				          <tr>
 				          	  <th>Código</th>
 				              <th>Nome</th>
-				              <th>Quantidade de dependentes</th>
+				              <th>Dependentes</th>
 				              <th>Salário bruto</th>
 				              <th>Vale transporte</th>
 				              <th>INSS</th>
@@ -40,16 +46,18 @@
 				        </thead>
 
 				        <tbody>
-				        	<tr>
-					        	<td>0</td>
-					            <td>Nome</td>
-					            <td>1</td>
-					            <td>R$2000,00</td>
-					            <td>R$192,00</td>
-					            <td>R$190,00</td>
-					            <td>R$300,00</td>
-					            <td>R$1000,00</td>
-				          	</tr>
+                        <?php while($rows_folhas = $resultado_folha->fetch(PDO::FETCH_ASSOC)){ ?>
+							<tr>
+								<td><?php echo $rows_folhas['cd_folha']; ?></td>
+								<td><?php echo $rows_folhas['nm_funcionario']; ?></td>
+								<td><?php echo $rows_folhas['qt_dependentes']; ?></td>
+								<td><?php echo $rows_folhas['vl_salario']; ?></td>
+                                <td><?php echo $rows_folhas['vale_transporte']; ?></td>
+                                <td><?php echo $rows_folhas['inss']; ?></td>
+                                <td><?php echo $rows_folhas['irrf']; ?></td>
+                                <td><?php echo $rows_folhas['vl_liquido']; ?></td>
+							</tr>
+							<?php } ?>
 
 				        </tbody>
 				      </table>
